5fa1aa824fb7d04bb18c781df37bb8b03556192f
Sai Nikhil Reddy Vatti <sainikhilvatti1605@gmail.com>
2024-04-12 11:19:37 -0500

    Added seo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.glb"],
+
+  assetsInclude: ["**/*.glb", "**/*.jpg", "**/*.png", "**/*.svg", "**/*.mp3"],
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
 });
